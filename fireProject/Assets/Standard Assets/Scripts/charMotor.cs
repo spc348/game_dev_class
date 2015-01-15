c3bc8244a5c27203081ed8e8f2adf0d1221a251e
spc348 <spc348@nyu.edu>
2015-01-15 01:20:29 -0500

    oh you know

--- fireProject/Assets/Standard Assets/Scripts/charMotor.cs
+++ fireProject/Assets/Standard Assets/Scripts/charMotor.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 using System.Collections;
 
-public class charMotor : MonoBehaviour {
+public class charMotor : MonoBehaviour
+{
 	
 	public static float speed = 10f;
 	public static float turnRate = 5f;
@@ -10,61 +11,64 @@ public class charMotor : MonoBehaviour {
 
 	
 	// Use this for initialization
-	void Start () {
-		transform.position = new Vector3(-14f,2f,0f);
+	void Start ()
+	{
+		transform.position = new Vector3 (-14f, 2f, 0f);
 		rigidbody.drag = 1f;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		bool grounded = Physics.Raycast(transform.position, -transform.up, rayLength);
-		if(grounded){
-		Physics.gravity= new Vector3 (0,-9f,0);	
+	void Update ()
+	{
+		bool grounded = Physics.Raycast (transform.position, -transform.up, rayLength);
+		if (grounded) {
+			Physics.gravity = new Vector3 (0, -9f, 0);	
 		}
 		
-		if (grounded == false){
-				Debug.Log(grounded);
-			 rigidbody.drag = 0f;	
-			 Physics.gravity = new Vector3 (0,-30f,0);
-			}
+		if (grounded == false) {
+			Debug.Log (grounded);
+			rigidbody.drag = 0f;	
+			Physics.gravity = new Vector3 (0, -30f, 0); 
+		}
 		
 	}
 	
-	void FixedUpdate(){
+	void FixedUpdate ()
+	{
 		
-		bool grounded = Physics.Raycast(transform.position, -transform.up, rayLength);
+		bool grounded = Physics.Raycast (transform.position, -transform.up, rayLength);
 		
-		if (Input.GetKeyDown(KeyCode.UpArrow) && grounded){
+		if (Input.GetKeyDown (KeyCode.UpArrow) && grounded) {
 			Debug.Log (grounded);
 			int i = 0;
-			for ( i = 0; i < 1; i++){
-			rigidbody.AddForce (transform.up * jumpRate, ForceMode.VelocityChange);
+			for (i = 0; i < 1; i++) {
+				rigidbody.AddForce (transform.up * jumpRate, ForceMode.VelocityChange);
 			}
 		}
 		
-		if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.UpArrow) && grounded){
+		if (Input.GetKey (KeyCode.LeftShift) && Input.GetKeyDown (KeyCode.UpArrow) && grounded) {
 			int i = 0;
-			for (i = 0; i < 1; i++){
-			rigidbody.AddForce (transform.up * jumpRate * 1.1f, ForceMode.VelocityChange);
+			for (i = 0; i < 1; i++) {
+				rigidbody.AddForce (transform.up * jumpRate * 1.1f, ForceMode.VelocityChange);
 			}
 			
 		
 		}
 		
-	if (Input.GetKey(KeyCode.RightArrow)){
+		if (Input.GetKey (KeyCode.RightArrow)) {
 			rigidbody.AddForce (transform.right * speed * Time.deltaTime, ForceMode.VelocityChange);
 		}
 		
-		if (Input.GetKey(KeyCode.LeftArrow)){
+		if (Input.GetKey (KeyCode.LeftArrow)) {
 			rigidbody.AddForce (-transform.right * speed * Time.deltaTime, ForceMode.VelocityChange);
 		}
 	
 
-	if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.LeftShift)){
+		if (Input.GetKey (KeyCode.RightArrow) && Input.GetKey (KeyCode.LeftShift)) {
 			rigidbody.AddForce (transform.right * speed * 2 * Time.deltaTime, ForceMode.VelocityChange);
 		}
 		
-		if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.LeftShift)){
+		if (Input.GetKey (KeyCode.LeftArrow) && Input.GetKey (KeyCode.LeftShift)) {
 			rigidbody.AddForce (-transform.right * speed * Time.deltaTime, ForceMode.VelocityChange);
 		}
 	
